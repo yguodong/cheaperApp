0189fe91d08e5d16851958cc5841de231f2ae394
qwer5454554 <1025472263@qq.com>
2017-12-26 21:12:17 +0800

    底部提交代码

---
+++ src/components/footer/AppFooter.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="app-footer">
+		<div class="footer-img">
+			<img src="http://ms1.sqkb.com/dist/image/sqkb_logo-158a4c28ab.png"/>
+		</div>
+		<div class="footer-text">
+			<div class="footer-text-title1">
+				<p class="left">打开&nbsp;</p>
+				<p class="right">省钱快报App</p>
+			</div>
+			<p class="footer-text-title2">买同样的商品，领更大额的券</p>
+		</div>
+		<div class="footer-right">
+			<img src="http://ms1.sqkb.com/dist/image/openApp-c97f1850ab.png"/>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'app-footer',
+		methods:{
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+</style>
