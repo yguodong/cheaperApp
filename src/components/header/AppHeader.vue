c31e2f83d0c8d564f2f828605ae8f0c2f4d40eba
qwer5454554 <1025472263@qq.com>
2017-12-27 21:54:28 +0800

    头部底部提交

---
+++ src/components/header/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="app-header">
+		<div class="top-ico">
+			<img src="http://img.cdn.daling.com/assets/web-touch/dl-icon1.b02ba9.png" />
+		</div>
+		<div class="top-text">
+			<em>前往新人专区 专享新人福利</em>
+			<i>连续荣获App Store最佳购物类应用</i>
+		</div>
+		<div class="top-right">
+			<span>立即下载</span>
+			<i class="yo-ico">&#xf077;</i>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'app-header',
+	}
+</script>
+
+<style lang="scss">
+
+</style>
