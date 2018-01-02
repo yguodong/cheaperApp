966cae16b957705ad72766e9f25e582119c5d2d9
qwer5454554 <1025472263@qq.com>
2018-01-02 16:36:14 +0800

    头部搜索框

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -1,22 +1,59 @@
 <template>
 	<div class="app-header">
-		<div class="top-ico">
-			<img src="http://img.cdn.daling.com/assets/web-touch/dl-icon1.b02ba9.png" />
+		<div class="header-top">
+
+			<div class="top-ico">
+				<img src="http://img.cdn.daling.com/assets/web-touch/dl-icon1.b02ba9.png" />
+			</div>
+			<div class="top-text">
+				<em>前往新人专区 专享新人福利</em>
+				<i>连续荣获App Store最佳购物类应用</i>
+			</div>
+			<div class="top-right">
+				<span>立即下载</span>
+				<i class="yo-ico">&#xf077;</i>
+			</div>
 		</div>
-		<div class="top-text">
-			<em>前往新人专区 专享新人福利</em>
-			<i>连续荣获App Store最佳购物类应用</i>
-		</div>
-		<div class="top-right">
-			<span>立即下载</span>
-			<i class="yo-ico">&#xf077;</i>
+		<div class="search">
+			<div class="search_input">
+				<i class="yo-ico">&#xf067;</i>
+				<input type="search" placeholder="搜索商品" data-search-input="">
+			</div>
+			<div class="search-menu">
+					<ul>
+						<li v-for="hdList in headerList">{{hdList.title}}</li>
+					</ul>
+			</div>
 		</div>
 	</div>
+
 </template>
 
 <script>
+	import axios from 'axios'
 	export default {
 		name: 'app-header',
+		data: function() {
+			return {
+				headerList: []
+			}
+		},
+		methods: {
+			getHeaderList() {
+				let that = this
+				axios.get('http://localhost:4000/dl/api/menu/homeChannel', {
+					params: {
+						_: Date.now()
+					}
+				}).then((response) => {
+					that.headerList = response.data.data
+				})
+			}
+		},
+		created() {
+			this.getHeaderList()
+		}
+
 	}
 </script>
 
